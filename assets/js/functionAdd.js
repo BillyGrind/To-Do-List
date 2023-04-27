ecbe1dc0b3b163f869f382a50fddc3695305c307
Anthony <denayeranthony1782@gmail.com>
2023-04-27 12:31:05 +0200

    SunnySide 2.0

--- assets/js/functionAdd.js
+++ assets/js/functionAdd.js
@@ -3,17 +3,17 @@ import { displayTask } from "./displayTask.js";
 export function functionAdd(array) {
   let buttonAdd = document.getElementsByClassName("button_add")[0];
   let inputText = document.getElementsByClassName("input_text")[0];
+  let form = document.querySelector("form");
 
-  buttonAdd.addEventListener("click", (event) => {
+  form.addEventListener("submit", (event) => {
     // localStorage.setItem("taskItem", JSON.stringify(array));
-    let inputValue = inputText.value;
-    array.push(inputValue);
-    displayTask(inputValue);
-    inputText.value = "";
-  });
-
-  inputText.addEventListener("keyup", (event) => {
+    event.preventDefault();
     if (event.key === "Enter") {
+      let inputValue = inputText.value;
+      array.push(inputValue);
+      displayTask(inputValue);
+      inputText.value = "";
+    } else {
       let inputValue = inputText.value;
       array.push(inputValue);
       displayTask(inputValue);
@@ -21,11 +21,3 @@ export function functionAdd(array) {
     }
   });
 }
-
-// buttonAdd.addEventListener("click", () => {
-//   localStorage.setItem("taskItem", JSON.stringify(array));
-//   let inputValue = inputText.value;
-//   array.push(inputValue);
-//   let taskItems = JSON.parse(localStorage.getItem("taskItem"));
-//   displayTask(inputValue);
-// });
